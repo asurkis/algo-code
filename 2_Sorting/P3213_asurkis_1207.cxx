5905e3c2b786eb807126e1773eddbb349eec3230
Anton Surkis <antonsurkis@gmail.com>
2020-03-06 20:48:47 +0300

    1207 and 1604

--- 2_Sorting/P3213_asurkis_1207.cxx
+++ 2_Sorting/P3213_asurkis_1207.cxx
@@ -1,10 +1,58 @@
-﻿#include <algorithm>
-#include <iostream>
-#include <limits>
-#include <string>
-#include <utility>
-#include <vector>
+﻿#include <iostream>
 
 using namespace std;
 
-int main() { return 0; }
+struct Point {
+  int id;
+  long long x;
+  long long y;
+};
+
+bool operator<(const Point &a, const Point &b) { return a.x * b.y < a.y * b.x; }
+
+void merge_sort(Point *src, Point *dst, int size) {
+  if (size < 2)
+    return;
+  int m = size / 2;
+  merge_sort(dst, src, m);
+  merge_sort(dst + m, src + m, size - m);
+
+  for (int i = 0; i + i < size - m; i++)
+    swap(src[m + i], src[size - i - 1]);
+
+  int l = 0;
+  int r = size - 1;
+  for (int i = 0; i < size; i++)
+    dst[i] = src[src[r] < src[l] ? r-- : l++];
+}
+
+int N;
+Point points[10000];
+Point buffer[10000];
+
+int main() {
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    points[i].id = i + 1;
+    cin >> points[i].x >> points[i].y;
+  }
+
+  int pivot = 0;
+  for (int i = 1; i < N; i++) {
+    if (points[i].x < points[pivot].x)
+      pivot = i;
+  }
+
+  swap(points[0], points[pivot]);
+  for (int i = 1; i < N; i++) {
+    points[i].x -= points[0].x;
+    points[i].y -= points[0].y;
+  }
+
+  for (int i = 0; i < N; i++)
+    buffer[i] = points[i];
+  merge_sort(buffer + 1, points + 1, N - 1);
+  cout << points[0].id << ' ' << points[N / 2].id << endl;
+
+  return 0;
+}
--- 2_Sorting/P3213_asurkis_1604.cxx
+++ 2_Sorting/P3213_asurkis_1604.cxx
@@ -1,10 +1,73 @@
-﻿#include <algorithm>
-#include <iostream>
-#include <limits>
-#include <string>
-#include <utility>
-#include <vector>
+﻿#include <iostream>
 
 using namespace std;
 
-int main() { return 0; }
+struct Sign {
+  int id;
+  int count;
+};
+
+bool operator<(const Sign &a, const Sign &b) { return a.count < b.count; }
+
+void merge_sort(Sign *src, Sign *dst, int size) {
+  if (size < 2)
+    return;
+  int m = size / 2;
+  merge_sort(dst, src, m);
+  merge_sort(dst + m, src + m, size - m);
+
+  for (int i = 0; i + i < size - m; i++)
+    swap(src[m + i], src[size - i - 1]);
+
+  int l = 0;
+  int r = size - 1;
+  for (int i = 0; i < size; i++)
+    dst[i] = src[src[l] < src[r] ? r-- : l++];
+}
+
+int k;
+Sign n[10000];
+Sign buf[10000];
+
+int main() {
+  cin >> k;
+  int total = 0;
+  for (int i = 0; i < k; i++) {
+    n[i].id = i + 1;
+    cin >> n[i].count;
+    total += n[i].count;
+    buf[i] = n[i];
+  }
+
+  merge_sort(buf, n, k);
+
+  int current = 0;
+  while (current < total) {
+    cout << n[0].id << ' ';
+    n[0].count--;
+    current++;
+    if (k == 1)
+      continue;
+
+    if (n[0].count + 1 == n[1].count) {
+      for (int i = 1; i < k; i++) {
+        if (n[0].count + 1 == n[i].count) {
+          cout << n[i].id << ' ';
+          n[i].count--;
+          current++;
+        }
+      }
+    } else if (n[1].count) {
+      cout << n[1].id << ' ';
+      n[1].count--;
+      current++;
+
+      int i = 1;
+      while (i + 1 < k && n[i].count < n[i + 1].count) {
+        swap(n[i], n[i + 1]);
+        i++;
+      }
+    }
+  }
+  return 0;
+}
