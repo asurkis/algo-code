005a404d986b641debafca0538b07b8f23450428
Anton Surkis <antonsurkis@gmail.com>
2020-03-07 16:52:06 +0300

    1322 and 1726

--- 2_Sorting/P3213_asurkis_1322.cxx
+++ 2_Sorting/P3213_asurkis_1322.cxx
@@ -1,10 +1,50 @@
 ﻿#include <algorithm>
 #include <iostream>
-#include <limits>
-#include <string>
-#include <utility>
-#include <vector>
 
 using namespace std;
 
-int main() { return 0; }
+char chr[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz";
+int ord[256];
+
+struct Char {
+  int pos;
+  char val;
+};
+
+char last_col[100001];
+Char chars[100000];
+
+bool operator<(const Char &a, const Char &b) {
+  if (ord[a.val] == ord[b.val])
+    return a.pos < b.pos;
+  return ord[a.val] < ord[b.val];
+}
+
+int main() {
+  for (int i = 0; i < 256; i++)
+    ord[i] = -1;
+  for (int i = 0; chr[i] != '\0'; i++)
+    ord[chr[i]] = i;
+
+  int K;
+  cin >> K >> last_col;
+
+  int n = 0;
+  while (last_col[n] != '\0') {
+    chars[n].pos = n;
+    chars[n].val = last_col[n];
+    n++;
+  }
+
+  if (n == 0)
+    return 0;
+
+  sort(chars, chars + n);
+
+  K--;
+  for (int i = 0; i < n; i++)
+    cout << last_col[K = chars[K].pos];
+  cout << endl;
+
+  return 0;
+}
--- 2_Sorting/P3213_asurkis_1726.cxx
+++ 2_Sorting/P3213_asurkis_1726.cxx
@@ -1,10 +1,44 @@
-﻿#include <algorithm>
-#include <iostream>
-#include <limits>
-#include <string>
-#include <utility>
-#include <vector>
+﻿#include <iostream>
 
 using namespace std;
 
-int main() { return 0; }
+typedef long long Long;
+
+Long n;
+Long x[100000];
+Long y[100000];
+Long buf[100000];
+
+void merge_sort(Long *src, Long *dst, Long size) {
+  if (size < 2)
+    return;
+  Long m = size / 2;
+  merge_sort(dst, src, m);
+  merge_sort(dst + m, src + m, size - m);
+
+  for (Long i = 0; i + i < size - m; i++)
+    swap(src[m + i], src[size - i - 1]);
+
+  Long l = 0;
+  Long r = size - 1;
+  for (Long i = 0; i < size; i++)
+    dst[i] = src[src[r] < src[l] ? r-- : l++];
+}
+
+int main() {
+  cin >> n;
+  for (Long i = 0; i < n; i++)
+    cin >> x[i] >> y[i];
+  for (Long i = 0; i < n; i++)
+    buf[i] = x[i];
+  merge_sort(buf, x, n);
+  for (Long i = 0; i < n; i++)
+    buf[i] = y[i];
+  merge_sort(buf, y, n);
+
+  Long sum = 0;
+  for (Long i = 1; i < n; i++)
+    sum += i * (n - i) * (x[i] - x[i - 1] + y[i] - y[i - 1]);
+  cout << 2 * sum / n / (n - 1) << endl;
+  return 0;
+}
