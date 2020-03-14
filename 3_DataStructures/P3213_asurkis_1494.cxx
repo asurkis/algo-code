932cdab8e6e3a11fdb4bea5d62144759d0cf0ef8
Anton Surkis <antonsurkis@gmail.com>
2020-03-14 11:23:06 +0300

    1494

--- 3_DataStructures/P3213_asurkis_1494.cxx
+++ 3_DataStructures/P3213_asurkis_1494.cxx
@@ -1,1 +1,27 @@
-﻿int main() { return 0; }
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<int> stack;
+  int ball = 1;
+  for (int i = 0; i < N; ++i) {
+    int top;
+    cin >> top;
+    if (empty(stack))
+      stack.push_back(ball++);
+    if (stack.back() > top) {
+      cout << "Cheater" << endl;
+      return 0;
+    }
+    while (stack.back() < top)
+      stack.push_back(ball++);
+    stack.pop_back();
+  }
+  cout << "Not a proof" << endl;
+  return 0;
+}
